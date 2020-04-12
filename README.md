02f63288a3732ba176718d1678528f5c59a19cff
MohanedKassim <aliyyi89@gmail.com>
2020-04-12 16:11:10 -0500

    added read me

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 [Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
 
 ## Description
+-Goal is to capture data from user input and pass it to both array and get it to append to the DOM in form of table. 
+-Further more employee monthly salary should be tracked as well as putting visual warning once the max of $20,000 is reached. 
+-User shuld be able to delete an employee from the DOM and afterwards the array should be updated accordingly. 
 
-Your project description goes here. What problem did you solve? How did you solve it?
 
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
--- scripts/client.js
+++ scripts/client.js
@@ -7,8 +7,9 @@ function onReady() {
     $('tbody').on('click', '.deleteBtn', deleteItem)
      $('.submitBtn').on('click', monthlyExpense);
 }
-
+//captures input data
 function addToList() {
+    //create object that will be passed to employee array.
     let itemEmpArray = {
         firstName: $('.firstNameInput').val(),
         lastName: $('.lastNameInput').val(),
@@ -16,20 +17,26 @@ function addToList() {
         title: $('.titleInput').val(),
         Salary: $('.salaryInput').val()
     }
+    //pushes input data to the employee array.
     employee.push(itemEmpArray)
    
 
     
 }
 
+//monthly expense. 
 function monthlyExpense(){
+    //cost variable 
     let totalCost = 0;
+    //where totalCost will go on the DOM
     let el = $('.monthlyDisplay');
     el.empty();
+    //looping through the array to find the salary of employee and assign it to totalCost.
     for(let i = 0; i < employee.length; i++) {
         totalCost += Math.round(employee[i].Salary) /12;
         el.text(Math.round(totalCost));
         //console.log(totalCost)
+        //condition for monthly cost max
         if(totalCost > 20000) {
             $('.monthlyDisplay').addClass('monthlyDisplayWarning')
         }else{
@@ -39,12 +46,12 @@ function monthlyExpense(){
      
 }
 
-
+//from input to DOM
 function displayToDom() {
     let el = $('table tbody')
     el.empty();
     for (let i = 0; i < employee.length; i++) {
-        // append each item to ul
+        // append each item as tr to tbody. .
         el.append(
         `<tr>
             <td>${ employee[i].firstName} </td>
@@ -64,12 +71,14 @@ function displayToDom() {
     }//end of loop
 }
 
+//removing from DOM
 function deleteItem() {
-    
+        //goal is to remove item clicked from table and employee array. 
         let item = $(this).closest('tr');
+        //deleting from array.
         employee.splice(item.index(), 1);
+        //removing from table.
         item.remove();
-         //addToList();
         monthlyExpense();
         displayToDom();
         //console.log(employee);
